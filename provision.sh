b2dddd823b8436f7b13f1127977435a96e889047
twilliams9397 <twilliams@spartaglobal.com>
2021-08-02 16:05:10 +0100

    Vagrantfile and provision added

---
+++ provision.sh
@@ -0,0 +1,9 @@
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > \
+    /etc/apt/sources.list.d/jenkins.list'
+sudo apt-get update
+sudo apt-get install jenkins
+
+sudo apt-get update
+sudo apt-get search openjdk
+sudo apt-get install openjdk-11-jdk
