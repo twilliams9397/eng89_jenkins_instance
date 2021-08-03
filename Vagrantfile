cca9cfa5fe1f6374515be6c3ee44804f4538260b
twilliams9397 <twilliams@spartaglobal.com>
2021-08-03 09:23:50 +0100

    provision edit

--- Vagrantfile
+++ Vagrantfile
@@ -2,6 +2,6 @@ Vagrant.configure("2") do |config|
 
 	config.vm.box = "hashicorp/bionic64"
 	config.vm.network "private_network", ip: "192.168.10.100"
-	config.vm.provision "shell", path: "provision.sh"
+	# config.vm.provision "shell", path: "provision.sh"
 
 end
--- provision.sh
+++ provision.sh
@@ -1,7 +1,6 @@
 sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt install openjdk-11-jdk -y
-java -version
 wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
 sudo apt-get update -y
 sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ >> /etc/apt/sources.list.d/jenkins.list'
