a96243bfb796d7534b2f703ef8ce579a320e266a
twilliams9397 <twilliams@spartaglobal.com>
2021-08-03 10:03:01 +0100

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -4,4 +4,5 @@ sudo apt install openjdk-11-jdk -y
 wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
 sudo apt-get update -y
 sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ >> /etc/apt/sources.list.d/jenkins.list'
+sudo apt-get update -y
 sudo apt-get install jenkins -y
